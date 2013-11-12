7d1c643ac1fecda4594e553adea4f607538217dc
Joona Lehtomäki <joona.lehtomaki@gmail.com>
2013-11-12 11:57:19 +0200

    Init

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='ztests',
+      version=version,
+      description="Testing harness for Zonation outputs",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      keywords='zonation test cbig',
+      author='Joona Lehtom\xc3\xa4ki',
+      author_email='joona.lehtomaki@gmail.com',
+      url='',
+      license='MIT',
+      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
